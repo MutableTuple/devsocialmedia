6a7ae3c5e5df61a29096a0d3ba3f5860268c0c82
MutableTuple <mutabletuple39@gmail.com>
2024-08-24 08:19:58 +0530

    fixed minor ui

--- app/_components/Navbar.jsx
+++ app/_components/Navbar.jsx
@@ -6,9 +6,13 @@ import Link from "next/link";
 export default function Navbar() {
   return (
     <div className="flex justify-between items-center bg-white py-4 px-4">
-      <div>
-        <h1 className="font-black">LOGO</h1>
-      </div>
+      <Link href={"/"}>
+        <div>
+          <h1 className="font-black underline underline-offset-4 hover:text-green-800">
+            💻Social.Dev
+          </h1>
+        </div>
+      </Link>
       <div className="flex gap-2">
         <Link href={"/user/notifications"}>
           <IconContainer>
